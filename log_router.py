a60818863dcff44f62f12c8bad41e86dbb155279
Pete Wildsmith <pete@dabapps.com>
2017-01-18 16:29:41 +0000

    add buildspec

---
+++ log_router.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+import zlib
+import base64
+import json
+import boto3
+import logging
+import pyjq
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+lambda_client = boto3.client('lambda')
+
+ROUTES = {
+    'foo': '',
+}
+
+def decode_event(event):
+    return base64.b64decode(event['awslogs']['data'])
+
+def decompress_event_data(compressed_event_data):
+    return zlib.decompress(compressed_event_data, 31)
+
+def deserialize_event_data(event_data):
+    return json.loads(event_data)
+
+def unpack_event(event):
+    zipped_data = decode_event(event)
+    serialized_data = decompress_event_data(zipped_data)
+    return deserialize_event_data(serialized_data)
+
+def lambda_handler(event, context):
+    event_data = unpack_event(event)
+    if event_data['messageType'] != 'DATA_MESSAGE':
+        return
+
+    log_group = event_data['logGroup']
+    log_events = event_data['logEvents']
+    for event in log_events:
+        event['logGroup'] = log_group
+        for log_filter, sub_handler in ROUTES.items():
+            if matches(log_filter, event):                
+                route(sub_handler, log_group, event)
+        
+def route(handler, event):
+    logger.info("would be invoking {} here but mocked for now".format(handler))
+    return
+    lambda_client.invoke(
+        FunctionName=route,
+        InvocationType='Event',
+        Payload=json.dumps(event),
+    )
+
+def matches(log_filter, event):
+   return True
+
